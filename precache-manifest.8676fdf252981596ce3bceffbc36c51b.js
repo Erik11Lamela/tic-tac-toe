6160af781ecd0999dd80a7326dd72b448b4d0ef8
Erik11Lamela <65742900+Erik11Lamela@users.noreply.github.com>
2020-10-16 16:36:29 +0500

    Updates

---
+++ precache-manifest.8676fdf252981596ce3bceffbc36c51b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63acafee9e67969563f31487c8d2ba53",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f13b14bb47c0aeea5c7f",
+    "url": "/tic-tac-toe/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "549dcba55c3cf5a3ec3d",
+    "url": "/tic-tac-toe/static/css/main.271c2318.chunk.css"
+  },
+  {
+    "revision": "f13b14bb47c0aeea5c7f",
+    "url": "/tic-tac-toe/static/js/2.e422752e.chunk.js"
+  },
+  {
+    "revision": "effe4e92e8658ad9dfbf53e804a021fb",
+    "url": "/tic-tac-toe/static/js/2.e422752e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "549dcba55c3cf5a3ec3d",
+    "url": "/tic-tac-toe/static/js/main.aa18a554.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
